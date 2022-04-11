05d49889c8e119b5d0b0d0bac60faef43efd7c62
LouisNarcis29 <Barbu.Narcis@outlook.com>
2022-04-12 01:25:57 +0300

    Create round function
    I've added a function called round to check the input from the player
    and from the computer to determine the result.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,3 +5,25 @@ function computerPlay() {
   return result;
 }
 
+function round(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  let result;
+
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    result = `You won because ${playerSelection} beats ${computerSelection}!`;
+  } else if (
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "paper" && playerSelection == "rock") ||
+    (computerSelection == "scissors" && playerSelection == "paper")
+  ) {
+    result = `You lose because ${computerSelection} beats ${playerSelection}!`;
+  } else if (playerSelection == computerSelection) {
+    result = `It's a tie!`;
+  }
+  return result;
+}
