3243fc47c9e756c82ff33599a70100a6eccf1d62
LouisNarcis29 <Barbu.Narcis@outlook.com>
2022-04-12 14:46:32 +0300

    Add function to validate the answer Add game funcion
    This is the project without CSS added yet.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,4 @@
+//randomly select an input for the computer
 function computerPlay() {
   let computerOptions = ["Rock", "Paper", "Scissors"];
   let randomNumber = Math.floor(Math.random() * computerOptions.length);
@@ -5,25 +6,70 @@ function computerPlay() {
   return result;
 }
 
+//all cases for a round
 function round(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
-  let result;
+  let result = 0;
 
   if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "scissors" && computerSelection == "paper")
   ) {
-    result = `You won because ${playerSelection} beats ${computerSelection}!`;
+    result = 1;
   } else if (
     (computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "paper" && playerSelection == "rock") ||
     (computerSelection == "scissors" && playerSelection == "paper")
   ) {
-    result = `You lose because ${computerSelection} beats ${playerSelection}!`;
+    result = 2;
   } else if (playerSelection == computerSelection) {
-    result = `It's a tie!`;
+    result = 3;
   }
   return result;
 }
+
+let score = 0;
+
+//output for cases and calculate the score
+function validate(roundResult, playerSelection, computerSelection) {
+  switch (roundResult) {
+    case 1:
+      console.log(
+        `You won because ${playerSelection} beats ${computerSelection}!`
+      );
+      score += 1;
+      break;
+    case 2:
+      console.log(
+        `You lose because ${computerSelection} beats ${playerSelection}!`
+      );
+      score -= 1;
+      break;
+    case 3:
+      console.log(`It's a tie!`);
+      break;
+  }
+}
+
+//get number of rounds
+let numberOfRounds = Number(prompt("How many rounds do you want to play"));
+
+//main function
+function game(numberOfRounds) {
+  let computerSelection;
+  let playerSelection;
+  let roundResult;
+  let i;
+
+  for (i = 0; i < numberOfRounds; i++) {
+    computerSelection = computerPlay();
+    playerSelection = prompt("What do you choose?");
+    roundResult = round(playerSelection, computerSelection);
+    validate(roundResult, playerSelection, computerSelection);
+  }
+  console.log(`Game finished, the final score is ${score}`);
+}
+
+game(numberOfRounds);
